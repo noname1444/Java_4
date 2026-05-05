c890af306a97d0cf8c8bdc77aad7a747eb300b14
noname1444 <silviagavrilenko@gmail.com>
2026-05-05 17:56:39 +0300

    Create script.js

---
+++ Джава 4/script.js
@@ -0,0 +1,63 @@
+let message = "hello"; //string
+let number = 123; //number
+let index = null; //null
+let say = 'Hola seniorita'; //string
+let pi = 3.1415; //number
+let check = true; //boolean
+let age; //undefined
+let milisecond = 0.001; //number
+let button = false; //boolean
+
+let IsBool = 'yes'; //string
+let TrueNull = '0'; //string
+let thisIsStr = true; //boolean
+let TypeNull; //undefined
+let IsInt = 'number'; //string
+let UserName = 0; //number
+let IsTrue = null; //null
+
+let name = "Федосья Ивановна"; //string
+let text =  `Привет, ${name}!`; //string 
+let isGreater = 2 > 1; //boolean
+let addition = 3 + 5; //number
+let isLess = 1 < 1; //boolean
+let billion = 1e9; //number
+let IsMath = "`результат: ${1 + 2}`"; //string
+const OneLightYear = 94607304725808; //number
+
+let isEmpty = {}; //object
+const million = 1000000000; //number
+let cat = new Object(); //object
+let id = Symbol("id"); //symbol
+const Int = 12345678901234567890; //bigint
+let user = Symbol("user"); //symbol
+let nullObj = new Object(); //object
+
+let my_age = '13';
+my_age = Number(my_age) //number
+let Sasha = 'my_name';
+my_name = String(Sasha) //string
+let isBool = 'true';
+isBool = Boolean(isBool) //boolean
+let isFalse = 0;
+isFalse = Boolean(isFalse) //boolean
+let ThisNull = false;
+ThisNull = Number(ThisNull); //number
+let nullType = 0;
+nullType = null; //null
+let x = "6/2";
+x = Number(x) //number
+let y = "12%5";
+y = Number(y) //number
+let Example = `результат: ${x} ** {y}`;
+Example = `результат: ${x ** y}`; //string
+
+let NumberType = 13;
+let StrType = 10;
+let BoolType = 7;
+let ObjType = 3;
+let NullType = 3;
+let undefinedType = 2; 
+let SymbType = 2;
+
+console.log(`И победителем становится команда: NumberType!`);
